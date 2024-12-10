5a416d0340d896d5b82c504527327184043ad0f7
Duda Leitte <161986325+dleitte@users.noreply.github.com>
2024-12-10 20:19:56 -0300

    Add files via upload

---
+++ numero primo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int divisor = 1, num = 0, primo = 0;
+    
+    printf("Insira um numero\n");
+    scanf("%d", &num);
+    
+    while(divisor<=num){
+        
+        if(num%divisor == 0){
+        primo++;}
+        divisor ++;
+        
+    }
+    
+    if(primo==2){
+        printf("É PRIMO");
+    }
+    else{
+        printf("NAO É PRIMO");
+    }
+    return 0;
+}
+
+
