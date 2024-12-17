7162f67cad8446fcb4b839ded9b262525e2bea01
Duda Leitte <161986325+dleitte@users.noreply.github.com>
2024-12-17 19:27:02 -0300

    Add files via upload

---
+++ listaq42.c
@@ -0,0 +1,17 @@
+/*42. Escreva um programa que imprima todos os números inteiros de 100 a 1 (em ordem
+decrescente).*/
+
+#include <stdio.h>
+
+int main()
+{
+    int contador;
+
+    for(contador=100; contador>0; contador--){
+
+      printf("%d ", contador);}
+
+
+
+    return 0;
+}
