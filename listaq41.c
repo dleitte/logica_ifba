f87c73ee9dd4db6153dee4bb378d9db9137635b5
Duda Leitte <161986325+dleitte@users.noreply.github.com>
2024-12-17 19:14:42 -0300

    Add files via upload

---
+++ listaq41.c
@@ -0,0 +1,15 @@
+/*41. Escreva um programa que imprima todos os números inteiros do intervalo fechado de 1 a
+100.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num = 1;
+    
+    for(num = 1; num <=100; num++){
+        printf("%d ", num);
+    }
+
+    return 0;
+}
