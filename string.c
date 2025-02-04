c89eddf73462c05fbfec38450bbf018e3fc86ffd
Duda Leitte <161986325+dleitte@users.noreply.github.com>
2025-02-04 19:32:16 -0300

    Add files via upload

---
+++ string.c
@@ -0,0 +1,24 @@
+/*String*/
+#include <stdio.h>
+#define MAX 256
+
+int main()
+{
+    char str [MAX];
+    str[0] = 'C';
+    str[1] = 'A';
+    str[2] = 'S';
+    str[3] = 'A';
+    str[4] = '\0';
+    
+    printf("a string e = %s", str);
+    return 0;
+    
+    ou
+    
+    printf("informe a string \n");
+    scanf("%s", palavra);
+    
+    printf("a string e = %s \n", palavra);
+}
+
