93acb42828f1d669d22d8831238e6813e5486606
Duda Leitte <161986325+dleitte@users.noreply.github.com>
2025-02-11 19:14:24 -0300

    Add files via upload

---
+++ matriz.c
@@ -0,0 +1,16 @@
+/*MATRIZ*/
+
+#include <stdio.h>
+
+int main()
+{
+    
+    //VARIAVEL = TENSOR DE GRAU 0
+    // ARRANJO = COLEÇÃO DE VARIAVEL (INDICE) = TENSOR DE GRAU 1
+    // MATRIZ = COLEÇÃO DE VARIAVEL MULTIDIMENSIONAL = TENSOR DE GRAU N 
+    //int matriz [3][3], float matriz [3] [3]
+    
+    printf("Hello World");
+
+    return 0;
+}
